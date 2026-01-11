2affa287052f57848b061193bd871452068c5876
Jiya Patil <jiyaapatil35@gmail.com>
2026-01-11 20:05:59 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); 
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -1861,3 +1861,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log("🚀 Server running on http://localhost:" + PORT)
 );
+
